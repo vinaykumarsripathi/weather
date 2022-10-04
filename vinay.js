9c4998f1ddb7fb08a090c223613e36937cd1e668
vinaykumarsripathi <vinaykumarsripathi22@gmail.com>
2022-10-04 15:55:11 +0530

    operators

---
+++ vinay.js
@@ -0,0 +1,37 @@
+// arthematic operators
+
+let a=15;
+let b=5;
+let c1=a+b;
+let c2=a-b;
+let c3=a*b;
+let c4=a/b;
+let c5=a%b;
+console.log(c1,c2,c3,c4,c5);
+
+
+
+//relational operator
+let x=10;
+let y=15;
+let s1=x>y;
+let s2=x<y;
+let s3=x<=y;
+let s4=x>=y;
+let s5=x|=y;
+console.log( "s1 is "+s1,"s2 is "+s2,"s3 is "+s3,"s4 is "+s4,"s5 is "+s5);
+
+
+
+//logical operators // assigning operator
+let p=24;
+let q=2;
+let r=p&&q;
+let s=p||q;
+let t=p<<q;
+let u=p>>q;
+let v=p^q;
+let w=p&q;
+console.log(r,s ,t,u,v,w);
+
+
